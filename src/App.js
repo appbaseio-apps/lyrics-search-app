f74ce7daf06f9cc8feecff0aa83b3cdef46e8bfc
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-03-25 11:03:43 +0530

    fix: styling issues

--- src/App.js
+++ src/App.js
@@ -17,57 +17,54 @@ function App() {
   const [appName, setAppName] = useState(APP_NAME.TEXT);
 
   return (
-    <div className="app-wrapper">
-      <ReactiveBase
-        app={appName}
-        enableAppbase
-        url="https://lYFCzg5ga:89fbdf16-216f-44c6-9191-900afaeaa74d@calm-river-nesrtpa-arc.searchbase.io"
-      >
-        <div className="row">
-          <div className="col max-width">
-            <ToggleButton
-              checked={appName === APP_NAME.TEXT}
-              handleChange={(checked) =>
-                setAppName(APP_NAME[checked ? "TEXT" : "KNN"])
-              }
-              labelBefore="KNN"
-              labelAfter="TEXT"
-            />
-            <ReactiveComponent
-              dataField={["Name", "Description"]}
-              componentId="lyric-input"
-            >
-              {(data) => {
-                return (
-                  <LyricsInput {...data} dataField={["Name", "Description"]} />
-                );
-              }}
-            </ReactiveComponent>
-          </div>
-          <div className="col">
-            <ReactiveList
-              infiniteScroll
-              dataField="Name"
-              showResultStats={true}
-              componentId="lyric-result"
-              pagination={false}
-              react={{ and: ["lyric-input"] }}
-              renderItem={(item) => {
-                return (
-                  <div className="lyric-item">
-                    <h4>{item.Name}</h4>
-                    <p>{item.Description}</p>
-                    <div class="popover__content">
-                      <p class="popover__message">{item.Description}</p>
-                    </div>
-                  </div>
-                );
-              }}
-            />
-          </div>
+    <ReactiveBase
+      app={appName}
+      enableAppbase
+      url="https://lYFCzg5ga:89fbdf16-216f-44c6-9191-900afaeaa74d@calm-river-nesrtpa-arc.searchbase.io"
+    >
+      <div className="row">
+        <div className="col max-width">
+          <ToggleButton
+            checked={appName === APP_NAME.TEXT}
+            handleChange={(checked) =>
+              setAppName(APP_NAME[checked ? "TEXT" : "KNN"])
+            }
+            labelBefore="KNN"
+            labelAfter="TEXT"
+          />
+          <ReactiveComponent
+            dataField={["Name", "Description"]}
+            componentId="lyric-input"
+          >
+            {(data) => {
+              return (
+                <LyricsInput {...data} dataField={["Name", "Description"]} />
+              );
+            }}
+          </ReactiveComponent>
         </div>
-      </ReactiveBase>
-    </div>
+        <div className="col">
+          <ReactiveList
+            size={3}
+            infiniteScroll
+            dataField="Name"
+            className="result-wrapper"
+            showResultStats={true}
+            componentId="lyric-result"
+            pagination={false}
+            react={{ and: ["lyric-input"] }}
+            renderItem={(item) => {
+              return (
+                <div className="lyric-item">
+                  <h4>{item.Name} </h4>
+                  <p>{item.Description}</p>{" "}
+                </div>
+              );
+            }}
+          />
+        </div>
+      </div>
+    </ReactiveBase>
   );
 }
 
